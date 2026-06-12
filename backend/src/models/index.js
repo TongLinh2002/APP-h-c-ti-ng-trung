658b202a2e994c39a13af137771cddbc4fecbff5
Tong. Emma (CVC) <Emma_Tong@compal.com>
2026-06-12 15:50:03 +0700

    feat(exam): register exam models + associations in index.js

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -6,6 +6,10 @@ const LessonQuestion = require('./LessonQuestion')
 const UserLessonHistory = require('./UserLessonHistory')
 const UserChallengeScore = require('./UserChallengeScore')
 const Download = require('./Download')
+const Exam = require('./Exam')
+const ExamSection = require('./ExamSection')
+const ExamQuestion = require('./ExamQuestion')
+const UserExamResult = require('./UserExamResult')
 
 User.hasMany(UserVocabularyProgress, { foreignKey: 'user_id' })
 Vocabulary.hasMany(UserVocabularyProgress, { foreignKey: 'vocabulary_id' })
@@ -23,4 +27,18 @@ UserLessonHistory.belongsTo(Lesson, { foreignKey: 'lesson_id' })
 User.hasMany(UserChallengeScore, { foreignKey: 'user_id' })
 UserChallengeScore.belongsTo(User, { foreignKey: 'user_id' })
 
-module.exports = { User, Vocabulary, UserVocabularyProgress, Lesson, LessonQuestion, UserLessonHistory, UserChallengeScore, Download }
+Exam.hasMany(ExamSection, { foreignKey: 'exam_id', as: 'sections', onDelete: 'CASCADE', hooks: true })
+ExamSection.belongsTo(Exam, { foreignKey: 'exam_id', as: 'exam' })
+ExamSection.hasMany(ExamQuestion, { foreignKey: 'section_id', as: 'questions', onDelete: 'CASCADE', hooks: true })
+ExamQuestion.belongsTo(ExamSection, { foreignKey: 'section_id', as: 'section' })
+User.hasMany(UserExamResult, { foreignKey: 'user_id' })
+UserExamResult.belongsTo(User, { foreignKey: 'user_id' })
+Exam.hasMany(UserExamResult, { foreignKey: 'exam_id' })
+UserExamResult.belongsTo(Exam, { foreignKey: 'exam_id' })
+
+module.exports = {
+  User, Vocabulary, UserVocabularyProgress,
+  Lesson, LessonQuestion, UserLessonHistory,
+  UserChallengeScore, Download,
+  Exam, ExamSection, ExamQuestion, UserExamResult,
+}
